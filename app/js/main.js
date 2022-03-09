0b4619f61e75ea7b318effb9e5f78d39159a00d1
Denis Vykhodtsev <denis-kadet@mail.ru>
2022-03-09 17:29:33 +0300

    fix/14109 - 09.03.2022

--- app/js/main.js
+++ app/js/main.js
@@ -60,4 +60,17 @@ $(document).ready(function() {
         activeIndex: false,
     });
 
+
+    $('.block-3__list-items').on('click', '.block-3__list-item', function() {
+        var lindex = $(this).index();
+        $(".block-3__slider").slick('slickGoTo', parseInt(lindex)); // меняем слад на нужный индекс
+        $('.block-3__list-item').removeClass('block-3__active'); // change class
+        $(this).addClass('block-3__active');
+    });
+    $('.block-3__slider').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+        var slideIndex = $(this).index();
+        $('.block-3__list-item').removeClass('block-3__active');
+        $('.block-3__list-item').eq(currentSlide).addClass('block-3__active')
+    });
+
 });
