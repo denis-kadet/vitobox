f7e1ed7135ba38394f89f8c387cfecc6d065fd0c
Denis Vykhodtsev <denis-kadet@mail.ru>
2022-03-09 11:54:27 +0300

    fix/14109 - 09.03.2022

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,38 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('.block-5__slider-wrap').slick({
         arrows: false,
         dots: false,
         arrows: true, // включение стрелок (если не нужны false)
         asNavFor: '.block-5__imagesnew_dotted', // указываем что навигация для слайдера будет отдельно (указываем класс куда вешаем навигацию),
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                }
-            },
-        ]
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+            }
+        }, ]
     });
 
     $('.block-5__imagesnew_dotted').slick({ // настройка навигации
         slidesToShow: 3, // указываем что нужно показывать 3 навигационных изображения
         asNavFor: '.block-5__slider-wrap', // указываем что это навигация для блока выше
         focusOnSelect: true // указываем что бы слайделось по клику
+    });
+
+
+    $('.block-6__slider-items').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 4,
+        // arrows: false,
+        // dots: false,
+        // arrows: true, // включение стрелок (если не нужны false)
+        // asNavFor: '.block-5__imagesnew_dotted', // указываем что навигация для слайдера будет отдельно (указываем класс куда вешаем навигацию),
+        // responsive: [{
+        //     breakpoint: 992,
+        //     settings: {
+        //         dots: true,
+        //     }
+        // }, ]
     });
 
 });
